07c2b927ead211ac0e2f87144b6ae7dbb267ac06
Sander Mendes <sandermendes@gmail.com>
2023-09-01 13:23:40 +0000

    Implemented swagger, the access link is: http://localhost:8080/swagger-ui

---
+++ src/main/java/com/sandermendes/springboot/config/SwaggerConfig.java
@@ -0,0 +1,69 @@
+package com.sandermendes.springboot.config;
+
+import java.util.Collections;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.builders.ResponseBuilder;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger.web.DocExpansion;
+import springfox.documentation.swagger.web.ModelRendering;
+import springfox.documentation.swagger.web.OperationsSorter;
+import springfox.documentation.swagger.web.TagsSorter;
+import springfox.documentation.swagger.web.UiConfiguration;
+import springfox.documentation.swagger.web.UiConfigurationBuilder;
+
+@Configuration
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfig {
+
+    private ApiInfo apiInfo() {
+        return new ApiInfo("Spring-Boot-App", "Some custom description of API.", "Main Application", "Terms of service",
+          new Contact("Sander Mendes", "https://github.com/sandermendes", "sandermendes@gmail.com"),
+          "License of API", "API license URL", Collections.emptyList());
+    }
+
+    @Bean
+    public Docket api() {
+        return new Docket(DocumentationType.SWAGGER_2)
+            .useDefaultResponseMessages(false)
+            .apiInfo(apiInfo())
+            .select()
+            .apis(RequestHandlerSelectors.basePackage("com.sandermendes.springboot.supplyFuel"))
+            .paths(PathSelectors.any())
+            .build();
+    }
+
+    /**
+     * SwaggerUI information
+     */
+
+    @Bean
+    UiConfiguration uiConfig() {
+        return UiConfigurationBuilder.builder()
+            .displayRequestDuration(true)
+            .deepLinking(true)
+            .displayOperationId(false)
+            .defaultModelsExpandDepth(1)
+            .defaultModelExpandDepth(1)
+            .defaultModelRendering(ModelRendering.EXAMPLE)
+            .displayRequestDuration(false)
+            .docExpansion(DocExpansion.NONE)
+            .filter(false)
+            .maxDisplayedTags(null)
+            .operationsSorter(OperationsSorter.ALPHA)
+            .showExtensions(false)
+            .tagsSorter(TagsSorter.ALPHA)
+            .supportedSubmitMethods(UiConfiguration.Constants.DEFAULT_SUBMIT_METHODS)
+            .validatorUrl(null)
+            .build();
+    }
+}
--- src/main/java/com/sandermendes/springboot/supplyFuel/SupplyFuelController.java
+++ src/main/java/com/sandermendes/springboot/supplyFuel/SupplyFuelController.java
@@ -6,10 +6,14 @@ import org.springframework.web.bind.annotation.*;
 
 import com.sandermendes.springboot.exception.EntityNotFoundException;
 
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+
 import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
+@Api(tags = "FuelSupply")
 @RestController
 @RequestMapping("api/v1/supply")
 public class SupplyFuelController {
@@ -17,21 +21,25 @@ public class SupplyFuelController {
     @Autowired
     private SupplyFuelService supplyFuelService;
 
+    @ApiOperation(value = "This method is used to list all the FuelSupply.")
     @GetMapping()
     public ResponseEntity<List<SupplyFuel>> getAll() {
         return ResponseEntity.ok().body(supplyFuelService.findAll());
     }
 
+    @ApiOperation(value = "This method is used to get one FuelSupply by ID")
     @GetMapping(value = "/{supplyFuelId}")
     public SupplyFuel getSupplyFuel(@PathVariable("supplyFuelId") Long supplyFuelId) throws EntityNotFoundException {
         return supplyFuelService.getSupplyFuelId(supplyFuelId);
     }
 
+    @ApiOperation(value = "This method is used to create one FuelSupply")
     @PostMapping
     public SupplyFuel createSupplyFuel(@Valid @RequestBody SupplyFuel supplyFuel) {
         return supplyFuelService.createSupplyFuel(supplyFuel);
     }
 
+    @ApiOperation(value = "This method is used to delete one FuelSupply by id")
     @DeleteMapping("/{id}")
     public ResponseEntity<Object> delete(@PathVariable final long id) {
         return ResponseEntity.ok().body(Map.of("message", supplyFuelService.deleteById(id)));
